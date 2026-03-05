c5a9aba41884f77a41cfb6e0c286abc84efccf9b
Swastik1005A <swastiksencollege@gmail.com>
2026-03-05 15:07:04 +0530

    2nd Commit

--- app/database.py
+++ app/database.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import sessionmaker , declarative_base
 from app.config import settings
 
 #Create database engine
-engine = create_engine(settings.DATABASE_URL , echo=True , pool_pre_ping=True)
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    pool_pre_ping=True,
+    connect_args={"ssl": {"ssl-mode": "REQUIRED"}}
+)
 
 # Create session Factory
 SessionLocal = sessionmaker(autocommit=False , autoflush=False , bind=engine)
