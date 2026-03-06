1f3ad09c39f0ed326d4bc77ed0f15677c16d841f
Swastik1005A <swastiksencollege@gmail.com>
2026-03-06 14:00:34 +0530

    sigh

--- app/api/candidate_routes.py
+++ app/api/candidate_routes.py
@@ -1,42 +1,42 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.database import SessionLocal
+from sqlalchemy import func
+
+from app.database import get_db
 from app.models.candidate import Candidate
 from app.models.score import Score
-from app.ml.skill_extractor import extract_skills_semantic
-from app.ml.experience_extractor import extract_experience_years
 
 router = APIRouter()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @router.get("/candidates")
 def get_candidates(db: Session = Depends(get_db)):
 
     rows = (
-        db.query(Candidate, Score)
+        db.query(
+            Candidate.id,
+            Candidate.name,
+            Candidate.email,
+            Candidate.experience_years,
+            Candidate.skills,
+            func.max(Score.final_score).label("score")
+        )
         .outerjoin(Score, Candidate.id == Score.candidate_id)
+        .group_by(Candidate.id)
         .all()
     )
 
     results = []
 
-    for candidate, score in rows:
+    for row in rows:
 
         results.append({
-            "candidate_id": candidate.id,
-            "name": candidate.name,
-            "email": candidate.email,
-            "experience": candidate.experience_years,
-            "skills": candidate.skills[:5] if candidate.skills else [],
-            "score": score.final_score if score else 0
+            "candidate_id": row.id,
+            "name": row.name,
+            "email": row.email,
+            "experience": row.experience_years,
+            "skills": row.skills[:5] if row.skills else [],
+            "score": row.score if row.score else 0
         })
 
     return results
